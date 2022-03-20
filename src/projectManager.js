bc53bee1d727767c3b3dd3674e847f0b1efb555e
Juan Ceballos <juandavi789@hotmail.com>
2022-03-20 14:30:25 -0500

    add project manager

---
+++ src/projectManager.js
@@ -0,0 +1,11 @@
+function ProjectManager(title) {
+  let projectsArray = [];
+
+  const addProject = (project) => projectsArray.push(project);
+
+  return {
+    projectsArray,
+    title,
+    addProject,
+  };
+}
